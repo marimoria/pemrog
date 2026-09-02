8bdaa1645efc99d85f5306d8fbe53e6709fbb816
marimoria <honiism@gmail.com>
2026-09-02 08:08:34 +0700

    Modified tabs

--- week2/02A.cpp
+++ week2/02A.cpp
@@ -3,53 +3,53 @@
 using namespace std;
 
 class MinumanKemasan {
-private:
-  string namaProduk;
-  int kapasitas;
-  int isiSekarang;
+  private:
+    string namaProduk;
+    int kapasitas;
+    int isiSekarang;
 
-public:
-  MinumanKemasan(string n, int k): namaProduk(n), kapasitas(k), isiSekarang(k) {}
+  public:
+    MinumanKemasan(string n, int k): namaProduk(n), kapasitas(k), isiSekarang(k) {}
 
-  void minum(int jumlah) {
-    isiSekarang -= jumlah;
-    if (isiSekarang < 0) {
-      isiSekarang = 0;
+    void minum(int jumlah) {
+      isiSekarang -= jumlah;
+      if (isiSekarang < 0) {
+        isiSekarang = 0;
+      }
     }
-  }
 
-  void isiUlang(int jumlah) {
-    isiSekarang += jumlah;
-    if (isiSekarang > kapasitas) {
-      isiSekarang = kapasitas;
-    } 
-  }
-
-  void show() {
-    cout << "Nama Produk : " << namaProduk << endl;
-    cout << "Kapasitas   : " << kapasitas << " ml" << endl;
-    cout << "Isi Sekarang: " << isiSekarang << " ml" << endl;
-  }
-
-  void setNamaProduk(string n) { namaProduk = n; }
-
-  void setKapasitas(int k) {
-    kapasitas = k;
-    if (isiSekarang > kapasitas) {
-      isiSekarang = kapasitas;
+    void isiUlang(int jumlah) {
+      isiSekarang += jumlah;
+      if (isiSekarang > kapasitas) {
+        isiSekarang = kapasitas;
+      } 
     }
-  }
 
-  void setIsiSekarang(int i) {
-    isiSekarang = i;
-    if (isiSekarang > kapasitas) {
-      isiSekarang = kapasitas;
+    void show() {
+      cout << "Nama Produk : " << namaProduk << endl;
+      cout << "Kapasitas   : " << kapasitas << " ml" << endl;
+      cout << "Isi Sekarang: " << isiSekarang << " ml" << endl;
     }
-  }
 
-  string getNamaProduk() { return namaProduk; }
-  int getKapasitas() { return kapasitas; }
-  int getIsiSekarang() { return isiSekarang; }
+    void setNamaProduk(string n) { namaProduk = n; }
+
+    void setKapasitas(int k) {
+      kapasitas = k;
+      if (isiSekarang > kapasitas) {
+        isiSekarang = kapasitas;
+      }
+    }
+
+    void setIsiSekarang(int i) {
+      isiSekarang = i;
+      if (isiSekarang > kapasitas) {
+        isiSekarang = kapasitas;
+      }
+    }
+
+    string getNamaProduk() { return namaProduk; }
+    int getKapasitas() { return kapasitas; }
+    int getIsiSekarang() { return isiSekarang; }
 };
 
 int main() {
